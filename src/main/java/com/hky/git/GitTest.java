1e22278e3b484ceb2ff178863549e36315cca277
Houky <hky1133@163.com>
2022-04-28 15:51:03 +0800

    master commit

--- src/main/java/com/hky/git/GitTest.java
+++ src/main/java/com/hky/git/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("hello,git!");
         System.out.println("hello,git!22222");
         System.out.println("hello,git!33333");
-        System.out.println("hello,git!44444");
+        System.out.println("master test");
     }
 }
