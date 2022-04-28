2ee90a62c2fef6972366c171487286250938ff9e
Houky <hky1133@163.com>
2022-04-28 15:48:48 +0800

    hot-fix commit2

--- src/main/java/com/hky/git/GitTest.java
+++ src/main/java/com/hky/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello,git!22222");
         System.out.println("hello,git!33333");
         System.out.println("hello,git!44444");
+        System.out.println("hit-fix test");
     }
 }
